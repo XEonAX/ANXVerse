de03fcfcb2c955be492762ec26840f26eb2945cc
XEonAX <sv.xeon@gmail.com>
2021-12-12 14:27:17 +0530

    Vayu Port

---
+++ zipit.bat
@@ -0,0 +1,15 @@
+xcopy /s /y .\out\ANXCamera.apk .\src\ANXVerse\system\priv-app\ANXCamera\
+xcopy /s /y .\src\ANXCamera12\frida\ANXCamera.json .\src\ANXVerse\system\priv-app\ANXCamera\lib\arm64\libanx.config.so
+xcopy /s /y .\src\ANXCamera12\frida\anx.js .\src\ANXVerse\system\etc\ANXCamera\anx.js
+
+del .\out\ANXVerse.zip
+del .\out\ANXVerse_*.zip
+del .\out\ANXCamera_*.zip
+
+for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
+echo %anxversion%
+
+"G:\Program Files\7-Zip\7z.exe" a -tzip .\out\ANXVerse.zip .\src\ANXVerse\*
+adb push .\out\ANXVerse.zip  /sdcard/zips/
+
+copy .\out\ANXVerse.zip /B .\out\ANXCamera_%anxversion%.zip
